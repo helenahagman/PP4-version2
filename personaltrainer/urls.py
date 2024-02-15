b3326116935f0f9c6f1c1d9505f90431432da16a
helenahagman <helena@rapibo.se>
2024-02-15 13:43:06 +0000

    updated urls.py

--- personaltrainer/urls.py
+++ personaltrainer/urls.py
@@ -1,16 +1,19 @@
-from django.urls import path
+from django.urls import path, include
 from . import views
+from django.contrib.auth.views import LoginView
+
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('accounts/', include('allauth.urls')),
     path('personal-trainer/', views.PersonalTrainerView.as_view(), name='personal_trainer'),
     path('members-only/', views.membersonlyView.as_view(), name='members_only'),
     path('member/', views.MemberView.as_view(), name='member'),
     path('book/', views.BookView.as_view(), name='book_session'),
     path('profile/', views.ProfileView.as_view(), name='profile_view'),
-    path('edit-profile/', views.EditProfileView.as_view(), name='edit_profile'),
-    path('signup/', views.signup, name='signup'),
-    path('login/', views.log_in, name='login'),
-    path('logout/', views.log_out, name='logout'),
+    path('accounts/edit-profile/', views.EditProfileView.as_view(), name='edit_profile'),
+    path('accounts/signup/', views.signup, name='signup'),
+    path('accounts/login/', views.log_in, name='login'),
+    path('accounts/logout/', views.log_out, name='logout'),
     path('contact/', views.contact_view, name='contact'),
 ]
