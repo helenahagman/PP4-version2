afdaa118f6a71e5a5671fa600c3883730b61ba73
helenahagman <helena@rapibo.se>
2024-02-18 13:46:50 +0000

    updated the booking request form with date and time selection

--- personaltrainer/forms.py
+++ personaltrainer/forms.py
@@ -1,9 +1,12 @@
+import datetime
 from django import forms
 from allauth.account.forms import SignupForm
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.contrib.auth.models import User
 from django.contrib.auth import authenticate, login, get_user_model
 from .models import Contact, Booking, Profile, MemberComment
+from django.utils import timezone
+
 
 User = get_user_model()
 
@@ -11,16 +14,31 @@ class ContactForm(forms.ModelForm):
     class Meta:
         model = Contact
         fields = ['name_contact', 'email', 'contact_message']
-            
+
+def time_choices():
+    start = datetime.time(8, 0)
+    end = datetime.time(22, 0)
+    interval = datetime.timedelta(minutes=30)
+
+    times = []
+    current = start
+    while current <= end:
+        times.append(current.strftime('%H:%M'))
+        current = (datetime.datetime.combine(datetime.date.today(), current) + interval).time()
+
+    return [(time, time) for time in times]            
 
 class BookingForm(forms.ModelForm):
+    time = forms.ChoiceField(choices=time_choices(), widget=forms.Select)
+
     class Meta:
         model = Booking
         fields = ['trainer_name', 'session_type', 'date', 'time', 'name', 'phonenumber', 'email', 'age', 'gender', 'message']
         widgets = {
             'message': forms.Textarea(attrs={'rows': 4}),
             'trainer_name': forms.Select(),
-            'sessions_type': forms.Select(),
+            'session_type': forms.Select(),
+            'date': forms.DateInput(attrs={'type': 'date', 'min': timezone.now().date().isoformat()}),
         }
 
 
